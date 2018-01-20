23f9ca5963fdfdc7454acb7996dc77caa2cca416
Matt Hivner <mh2296@uncw.edu>
2018-01-20 12:05:38 -0500

    add_matrices() works

--- driver.rb
+++ driver.rb
@@ -4,7 +4,27 @@ class Driver
   file_name = 'data1.txt'
   srand Time.now.to_i
 
+  def rand_fill(m)
+    (0..m.rows_count - 1).step do |row|
+      (0..m.cols_count - 1).step do |column|
+        m.matrix[row][column] = rand(0.0..10.0)
+      end
+    end
+  end
+
   def go
+    my_matrix = MattMatrix.new
+    rand_fill(my_matrix)
+    p my_matrix
+
+    my_second_matrix = MattMatrix.new
+    rand_fill(my_second_matrix)
+    p my_second_matrix
+
+    my_third_matrix = MattMatrix.new
+    my_matrix.add_matrices(my_second_matrix, my_third_matrix)
+
+    p my_third_matrix
 
   end
 end
--- matt_matrix.rb
+++ matt_matrix.rb
@@ -1,33 +1,41 @@
 =begin
+
 Look, this thing is kind of shit
 should probably redo with structs
+
+Creating a MattMatrix with no arguments gets you a 2x2 zero matrix.
+
 =end
 
 class MattMatrix
+  attr_accessor :rows_count, :cols_count, :matrix
 
-  attr_accessor :num_rows, :num_cols
-
-  def initialize(rows = 2, cols = 2)
-    @rows = rows
-    @cols = cols
-    zero_matrix
+  def initialize(*args)
+    # we got no arguments, just an empty matrix
+    if args.length.zero?
+      @rows_count = 2
+      @cols_count = 2
+      build(@rows_count, @cols_count)
+      zero_matrix
+    end
   end
 
-  def rows=(rows)
-    raise 'Must have at least 1 row' if rows < 1
-    @rows = rows
+  def rows_count=(rows_count)
+    raise 'Must have at least 1 row' if rows_count < 1
+    @rows_count = rows_count
   end
 
-  def cols=(cols)
-    raise 'Must have at least 1 column.' if cols < 1
-    @cols = cols
+  def cols_count=(cols_count)
+    raise 'Must have at least 1 column.' if cols_count < 1
+    @cols_count = cols_count
   end
 
-  # so, I hate the lack of *checking* done here
-  def MattMatrix.[](*rows)
-    @rows = rows.count
+  def matrix=(matrix)
+    @matrix = matrix
+  end
 
-
+  def build(r, c)
+    @matrix = Array.new(r) { Array.new(c) }
   end
 
   # this function won't work yet
@@ -40,10 +48,9 @@ class MattMatrix
   # fills all elements with zeros
   # this will destroy your matrix
   # don't call it on something important
-  # probably broken now
   def zero_matrix
-    (0..@rows - 1).each do |row|
-      (0..@cols - 1).each do |column|
+    (0..@rows_count - 1).each do |row|
+      (0..@cols_count - 1).each do |column|
         @matrix[row][column] = 0
       end
     end
@@ -55,8 +62,8 @@ class MattMatrix
   # don't call it on something important
   # probably broken now
   def make_identity
-    (0..@rows - 1).each do |row|
-      (0..@cols - 1).each do |column|
+    (0..@rows_count - 1).each do |row|
+      (0..@cols_count - 1).each do |column|
         if row == column
           @matrix[row][column] = 1
         else
@@ -72,24 +79,19 @@ class MattMatrix
   # ya, broke
   def conformable_add?(other_matrix)
     raise 'Must be a MattMatrix' unless other_matrix.class == MattMatrix
-    if @rows != other_matrix.rows
-      false
-    elsif @cols != other_matrix.cols
+    if @rows_count != other_matrix.rows_count || @cols_count != other_matrix.cols_count
       false
     else
       true
     end
   end
 
-  # need to be able to add conformable matrices
-  # broke
-  def +(other_matrix)
-    raise 'Must be conformable matrices' unless conformable_add?(other_matrix)
-    (0..@rows - 1).each do |row|
-      (0..@cols - 1).each do |column|
-        x = @matrix[row][column]
-        y = other_matrix[row][column]
-        x + y
+  # self + src = dest
+  def add_matrices(src, dest)
+    raise 'Must be conformable matrices' unless conformable_add? src
+    (0..@rows_count - 1).each do |row|
+      (0..@cols_count - 1).each do |column|
+        dest.matrix[row][column] = @matrix[row][column] + src.matrix[row][column]
       end
     end
   end
