24da03856f665d2b66fe186c0b44804eb4cb470e
matt h <mh2296@uncw.edu>
2018-01-30 09:15:34 -0500

    Project 1, question one complete.

--- project_one.rb
+++ project_one.rb
@@ -1,11 +1,15 @@
 require_relative 'matt_matrix'
 class ProjectOne
 
-  attr_accessor :class_one, :class_two
+  attr_reader :class_one, :class_two, :m_one, :m_two
 
   def initialize
+    m_one = []
+    m_two = []
     @class_one = class_one
     @class_two = class_two
+    @m_one = m_one
+    @m_two = m_two
   end
 
   def pull_data(file_name)
@@ -24,14 +28,39 @@ class ProjectOne
     end
   end
 
-  def print_data
+  def calc_mean_vectors
+    sum_one_one = 0.0
+    sum_one_two = 0.0
+    sum_two_one = 0.0
+    sum_two_two = 0.0
+    (0..class_one.length - 1).each do |row|
+      sum_one_one += @class_one[row][0]
+      sum_one_two += @class_one[row][1]
+      sum_two_one += @class_two[row][0]
+      sum_two_two += @class_two[row][1]
+    end
+    @m_one[0] = sum_one_one / @class_one.length
+    @m_one[1] = sum_one_two / @class_one.length
+    @m_two[0] = sum_two_one / @class_two.length
+    @m_two[1] = sum_two_two / @class_two.length
+  end
+
+  def print_all_data
     puts 'class_one length: ' + @class_one.length.to_s
     puts 'class_two length: ' + @class_two.length.to_s
     (0..@class_one.length - 1).each { |row| puts @class_one[row].to_s + ' ' + @class_two[row].to_s }
+    puts 'mean for class one, : ' + @m_one.to_s
+    puts 'mean for class two: ' + @m_two.to_s
+  end
+
+  def question_one
+    pull_data'data.txt'
+    calc_mean_vectors
+    puts 'mean for class one, : ' + @m_one.to_s
+    puts 'mean for class two: ' + @m_two.to_s
   end
 end
 
 my_project = ProjectOne.new
-my_project.pull_data('data.txt')
-my_project.print_data
+my_project.question_one
 
