17842b8eb8d7812af76ccc6029b9585dfdd2fe63
matt h <mh2296@uncw.edu>
2018-02-12 14:14:37 -0500

    crate_matrix(), get_num_rows(), get_num_cols(), and square?() all working

--- matrix_driver.rb
+++ matrix_driver.rb
@@ -25,11 +25,38 @@ class MatrixDriver
     puts square? n
   end
 
+  def test_zero_matrix
+    m = create_matrix(3, 3)
+    zero_matrix(m)
+    p m
+  end
+
+  def test_make_identity
+    m = create_matrix(4, 4)
+    make_identity(m)
+    p m
+  end
+
+  def test_print_matrix
+    m = create_matrix(4, 4)
+    make_identity(m)
+    print_matrix(m)
+  end
+
+  def test_augment_with_identity
+    m = create_matrix(5, 5)
+    zero_matrix(m)
+    print_matrix(m)
+    puts ''
+    augment_with_identity(m)
+    print_matrix(m)
+  end
+
   def scrap
-    m = create_matrix(1,3)
+    m = create_matrix(1, 3)
     puts get_num_rows(m)
   end
 end
 
 driver = MatrixDriver.new
-driver.test_square
+driver.test_augment_with_identity
--- matrix_masher.rb
+++ matrix_masher.rb
@@ -9,7 +9,7 @@ module MMasher
   end
 
   def get_num_cols(a)
-    raise 'Object is not a 2d Matrix.' unless a[0].class == Array
+    raise 'Object is not at least a 2d Matrix.' unless a[0].class == Array
     a[0].length
   end
 
@@ -17,5 +17,37 @@ module MMasher
     raise 'Object is not an Array.' unless a.class == Array
     raise 'Object is not a 2d Matrix.' unless a[0].class == Array
     a.length == a[0].length
+  end
+
+  def zero_matrix(a)
+    (0...a.length).each do |r|
+      (0...a[0].length).each do |c|
+        a[r][c] = 0
+      end
+    end
+  end
+
+  def make_identity(a)
+    raise 'Matrix is not square.' unless square?(a)
+    (0...a.length).each do |r|
+      (0...a[0].length).each do |c|
+        a[r][c] = 1 if r == c
+        a[r][c] = 0 if r != c
+      end
+    end
+  end
+
+  def print_matrix(a)
+    (0...a.length).each { |r| p a[r] }
+  end
+
+  def augment_with_identity(a)
+    new_c_count = a[0].length * 2
+    (0...a.length).each do |r|
+      (a.length...new_c_count).each do |c|
+        a[r][c] = 1 if r + a.length == c
+        a[r][c] = 0 if r + a.length != c
+      end
+    end
   end
 end
