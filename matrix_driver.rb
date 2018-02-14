deeeb1c8315d11495438e59be8a8b263624f0700
Matt <mh2296@uncw.edu>
2018-02-14 18:56:38 -0500

    gauss-jordan for revision not working

--- matrix_driver.rb
+++ matrix_driver.rb
@@ -2,6 +2,15 @@ load('matrix_masher.rb')
 
 class MatrixDriver
   include(MMasher)
+  def rand_fill(a)
+    srand
+    (0...a.length).step do |r|
+      (0...a[0].length).step do |c|
+        a[r][c] = rand(0.0..10.0)
+      end
+    end
+  end
+
   def test_create_matrix
     m = create_matrix(2, 3)
     puts m.class
@@ -82,6 +91,84 @@ class MatrixDriver
     puts conformable_mult?(o, p)
   end
 
+  def test_add_matrices
+    m = create_matrix(4, 4)
+    n = create_matrix(4, 4)
+    rand_fill(m)
+    rand_fill(n)
+    print_matrix(m)
+    puts ''
+    print_matrix(n)
+    puts ''
+    o = add_matrices(m, n)
+    print_matrix(o)
+  end
+
+  def test_sub_matrices
+    m = create_matrix(4, 4)
+    n = create_matrix(4, 4)
+    rand_fill(m)
+    rand_fill(n)
+    print_matrix(m)
+    puts ''
+    print_matrix(n)
+    puts ''
+    o = sub_matrices(m, n)
+    print_matrix(o)
+  end
+
+  def test_mult_matrices
+    m = create_matrix(2, 2)
+    n = create_matrix(2,2)
+    rand_fill(m)
+    rand_fill(n)
+    print_matrix(m)
+    puts ''
+    print_matrix(n)
+    puts ''
+    o = mult_matrices(m, n)
+    print_matrix(o)
+  end
+
+  def test_mult_matrix_scalar
+    m = create_matrix(3, 3)
+    n = 4
+    rand_fill(m)
+    print_matrix(m)
+    puts ''
+    o = mult_matrix_scalar(m, n)
+    print_matrix(o)
+  end
+
+  def test_interchange_rows
+    m = create_matrix(4, 4)
+    rand_fill(m)
+    print_matrix(m)
+    puts ''
+    interchange_rows(m, 1, 3)
+    print_matrix(m)
+  end
+
+  def test_mult_row_cons
+    m = create_matrix(3, 3)
+    rand_fill(m)
+    print_matrix(m)
+    puts ''
+    mult_row_constant(m, 1, 2)
+    print_matrix(m)
+  end
+
+  def test_gje
+    m = create_matrix(3, 4)
+    m[0] = [4.0, -2.0, 1.0, 11.0]
+    m[1] = [8.0, 5.0, -4.0, 14.0]
+    m[2] = [-3.0, 1.0, 5.0, 10.0]
+    print_matrix(m)
+    puts ''
+    n = gauss_jordan_elim(m)
+    print(n)
+  end
+
   def scrap
     m = create_matrix(1, 3)
     puts get_num_rows(m)
@@ -89,4 +176,4 @@ class MatrixDriver
 end
 
 driver = MatrixDriver.new
-driver.test_conformable_mult
+driver.test_gje
--- matrix_masher.rb
+++ matrix_masher.rb
@@ -65,4 +65,103 @@ module MMasher
   def conformable_mult?(a, b)
     a[0].length == b.length
   end
+
+  def add_matrices(a, b)
+    raise 'Matrices are not conformable for addition/subtraction.' unless conformable_add?(a, b)
+    apb = Array.new(a.length) { Array.new(a[0].length) }
+    (0...a.length).each do |r|
+      (0...a[0].length).each do |c|
+        apb[r][c] = a[r][c] + b[r][c]
+      end
+    end
+    apb
+  end
+
+  def sub_matrices(a, b)
+    raise 'Matrices are not conformable for addition/subtraction.' unless conformable_add?(a, b)
+    amb = Array.new(a.length) { Array.new(a[0].length) }
+    (0...a.length).each do |r|
+      (0...a[0].length).each do |c|
+        amb[r][c] = a[r][c] - b[r][c]
+      end
+    end
+    amb
+  end
+
+  def mult_matrices(a, b)
+    raise 'Matrices are not conformable for multiplication.' unless conformable_mult?(a, b)
+    atb = Array.new(a.length) { Array.new(b[0].length) }
+    zero_matrix(atb)
+    (0...a.length).each do |r|
+      (0...b[0].length).each do |c|
+        (0...a[0].length).each do |i|
+          atb[r][c] += a[r][i] * b[i][c]
+        end
+      end
+    end
+    atb
+  end
+
+  def mult_matrix_scalar(m, s)
+    atb = Array.new(m.length) { Array.new(m[0].length) }
+    zero_matrix(atb)
+    (0...a.length).each do |r|
+      (0...a[0].length).each do |c|
+        atb[r][c] = a[r][c] * s
+      end
+    end
+    atb
+  end
+
+  def mult_row_constant(m, r, s)
+    (0...m[r].length).each do |c|
+      m[r][c] *= s
+    end
+  end
+
+  def mult_vec_constant(r, s)
+    (0...r.length).each do |c|
+      r[c] *= s
+    end
+  end
+
+  def interchange_rows(m, r1, r2)
+    puts 'interchange_rows() called.'
+    t = Marshal.load(Marshal.dump(m[r1]))
+    m[r1] = Marshal.load(Marshal.dump(m[r2]))
+    m[r2] = Marshal.load(Marshal.dump(t))
+  end
+
+  def calc_pivot(m, i)
+    r = i
+    c = i
+    pi = -1
+    pv = 0.0
+    (r...m.length).each do |ri|
+      if m[ri][c].abs > pv
+        pv = m[ri][c]
+        pi = ri
+      end
+    end
+    pi
+  end
+
+  def gauss_jordan_elim(m)
+    gje = Array.new(m.length) { Array.new(m[0].length) }
+    zero_matrix(gje)
+    (0...m.length).each do |r|
+      p = calc_pivot(m, r)
+      raise 'No unique solution exists.' if p == -1
+      interchange_rows(m, r, p) if p > r
+      mult_row_constant(m, r, 1 / m[r][r])
+      (0...m.length).each do |i|
+        next if r == i
+        tv = m[r].clone
+        tc = m[i][r]
+        mult_vec_constant(tv, tc)
+        (0...m[i].length).each { |c| gje[i][c] -= tv[c] }
+      end
+    end
+    gje
+  end
 end
